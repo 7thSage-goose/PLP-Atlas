a6ef200125207f3be06deca3904b34ae4404b941
7thSage-goose <thandolwethundhlovu8@gmail.com>
2025-03-15 14:34:42 +0200

    "Atlas has been turned to a calculator."

--- Atlas.py
+++ Atlas.py
@@ -1,1 +1,13 @@
-print("Hello world")
+#This section assigns variables to the numbers and the function inputed by a user
+func=input("Enter a function(+,-,*,/):")
+num1=float(input("Enter a number:"))
+num2=float(input("Enter another number:"))
+#This section processes the function and prints the result for each possible function
+if func=="+":
+    print(f"{num1}+{num2}={num1+num2}")
+elif func=="-":
+    print(f"{num1}-{num2}={num1-num2}")
+elif func=="*":
+    print(f"{num1}*{num2}={num1*num2}")
+elif func=="/":
+    print(f"{num1}/{num2}={num1/num2}")
